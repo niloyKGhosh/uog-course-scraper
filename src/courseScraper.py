1c7597ae490c539a4a92c6f2cf554690a8ba65e7
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-24 00:01:24 -0500

    Extracted course titles, course restrictions and course credits

--- src/courseScraper.py
+++ src/courseScraper.py
@@ -1,19 +1,47 @@
 from bs4 import BeautifulSoup
 import requests
 
-allLinks = []
-pageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/'
+def courseContentToDb(link = ''):
 
-coursePage = requests.get(pageUrl)
-coursePgContents = coursePage.content
-soup = BeautifulSoup(coursePgContents, 'html.parser')
+    coursePageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/' + link
+    page = requests.get(coursePageUrl)
+    contents = page.content
+    courseSoup = BeautifulSoup(contents, 'html.parser')
+    courseDivs = courseSoup.find_all('div', {'class': 'course'})
 
-courseListDiv = soup.findAll('div', {'class': 'subnav'})
+    for a in courseDivs:
+        course = a.find('tr', {'class': 'title'}).get_text()
+        courseTitle = course.split()[0]
+        courseCredit = course.split()[-1][1:5]
 
-for a in courseListDiv:
-    courseListAnchors = a.findAll('a', href=True)
+        try:
+            restrictions = a.find('tr', {'class': 'restrictions'}).get_text().strip().replace('Restriction(s):', '').replace('\n', '')
+        except:
+            restrictions = "None"
+        
+        # print(restrictions)
+        
+def courseScraper():
+    
+    allLinks = []
+    pageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/'
 
-courseListAnchors.pop(0)
+    coursePage = requests.get(pageUrl)
+    coursePgContents = coursePage.content
+    soup = BeautifulSoup(coursePgContents, 'html.parser')
+    courseListDiv = soup.findAll('div', {'class': 'subnav'})
 
-for link in courseListAnchors:
-    allLinks.append(link['href'][2:])
+    for a in courseListDiv:
+        courseListAnchors = a.findAll('a', href = True)
+
+    courseListAnchors.pop(0)
+
+    for link in courseListAnchors:
+        allLinks.append(link['href'][2:])
+
+    courseContentToDb(allLinks[0])
+    # for link in allLinks:
+        # courseContentToDb(link)
+
+courseScraper()
+
