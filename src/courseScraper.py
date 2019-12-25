37ef113cdbfe0e4ca5c1dca9fa124e7729fa99e3
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-24 23:58:23 -0500

    extracted all the course prerequisites, restrictions, credits and titles and appended them in a list

--- src/courseScraper.py
+++ src/courseScraper.py
@@ -3,23 +3,46 @@ import requests
 
 def courseContentToDb(link = ''):
 
+    allCourseTitles = []
+    allCourseRestrictions = []
+    allCoursePrerequisites = []
+    allCourseCredits = []
+
     coursePageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/' + link
     page = requests.get(coursePageUrl)
     contents = page.content
     courseSoup = BeautifulSoup(contents, 'html.parser')
     courseDivs = courseSoup.find_all('div', {'class': 'course'})
+    
 
     for a in courseDivs:
         course = a.find('tr', {'class': 'title'}).get_text()
         courseTitle = course.split()[0]
         courseCredit = course.split()[-1][1:5]
 
-        try:
+        if a.find('tr', {'class': 'restrictions'}):
             restrictions = a.find('tr', {'class': 'restrictions'}).get_text().strip().replace('Restriction(s):', '').replace('\n', '')
-        except:
+        else:
             restrictions = "None"
         
-        # print(restrictions)
+        if a.find('tr', {'class': 'prereqs'}):
+            prereq = a.find('tr', {'class': 'prereqs'}).get_text().strip().replace('Prerequisite(s):', '').replace('\n','')
+        else:
+            prereq = "None"
+        
+        allCourseTitles.append(courseTitle)
+        allCourseCredits.append(courseCredit)
+        allCoursePrerequisites.append(prereq)
+        allCourseRestrictions.append(restrictions)
+
+    # print(allCourseTitles, len(allCourseTitles))
+    # print()
+    # print(allCourseCredits, len(allCourseCredits))
+    # print()
+    # print(allCoursePrerequisites, len(allCoursePrerequisites))
+    # print()
+    # print(allCourseRestrictions, len(allCourseRestrictions))
+      
         
 def courseScraper():
     
@@ -39,9 +62,10 @@ def courseScraper():
     for link in courseListAnchors:
         allLinks.append(link['href'][2:])
 
+    # print(allLinks)
     courseContentToDb(allLinks[0])
     # for link in allLinks:
-        # courseContentToDb(link)
+    #     courseContentToDb(link)
 
 courseScraper()
 
