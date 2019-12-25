4ce05013c5896153b06872d6d5c2d6232d57cebb
NiloyGhosh <55622147+niloyKGhosh@users.noreply.github.com>
2019-12-25 00:33:57 -0500

    All course data now loads to a csv file. Fixed a major bug where a function call replaces every data in the csv file, every time it is called

--- src/courseScraper.py
+++ src/courseScraper.py
@@ -1,52 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-def courseContentToDb(link = ''):
-
+def courseScraper():
+    allLinks = []
     allCourseTitles = []
     allCourseRestrictions = []
     allCoursePrerequisites = []
     allCourseCredits = []
 
-    coursePageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/' + link
-    page = requests.get(coursePageUrl)
-    contents = page.content
-    courseSoup = BeautifulSoup(contents, 'html.parser')
-    courseDivs = courseSoup.find_all('div', {'class': 'course'})
-    
-
-    for a in courseDivs:
-        course = a.find('tr', {'class': 'title'}).get_text()
-        courseTitle = course.split()[0]
-        courseCredit = course.split()[-1][1:5]
-
-        if a.find('tr', {'class': 'restrictions'}):
-            restrictions = a.find('tr', {'class': 'restrictions'}).get_text().strip().replace('Restriction(s):', '').replace('\n', '')
-        else:
-            restrictions = "None"
-        
-        if a.find('tr', {'class': 'prereqs'}):
-            prereq = a.find('tr', {'class': 'prereqs'}).get_text().strip().replace('Prerequisite(s):', '').replace('\n','')
-        else:
-            prereq = "None"
-        
-        allCourseTitles.append(courseTitle)
-        allCourseCredits.append(courseCredit)
-        allCoursePrerequisites.append(prereq)
-        allCourseRestrictions.append(restrictions)
-
-    # print(allCourseTitles, len(allCourseTitles))
-    # print()
-    # print(allCourseCredits, len(allCourseCredits))
-    # print()
-    # print(allCoursePrerequisites, len(allCoursePrerequisites))
-    # print()
-    # print(allCourseRestrictions, len(allCourseRestrictions))
-      
-        
-def courseScraper():
-    
-    allLinks = []
     pageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/'
 
     coursePage = requests.get(pageUrl)
@@ -57,15 +19,45 @@ def courseScraper():
     for a in courseListDiv:
         courseListAnchors = a.findAll('a', href = True)
 
-    courseListAnchors.pop(0)
+    courseListAnchors.pop(0)    # Getting rid of index.shtml
 
     for link in courseListAnchors:
         allLinks.append(link['href'][2:])
 
-    # print(allLinks)
-    courseContentToDb(allLinks[0])
-    # for link in allLinks:
-    #     courseContentToDb(link)
+    for link in allLinks:
+        coursePageUrl = 'https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/' + link
+        page = requests.get(coursePageUrl)
+        contents = page.content
+        courseSoup = BeautifulSoup(contents, 'html.parser')
+        courseDivs = courseSoup.find_all('div', {'class': 'course'})
+        
+        for a in courseDivs:
+            course = a.find('tr', {'class': 'title'}).get_text()
+            courseTitle = course.split()[0]
+            courseCredit = course.split()[-1][1:5]
+
+            if a.find('tr', {'class': 'restrictions'}):
+                restrictions = a.find('tr', {'class': 'restrictions'}).get_text().strip().replace('Restriction(s):', '').replace('\n', '')
+            else:
+                restrictions = "None"
+            
+            if a.find('tr', {'class': 'prereqs'}):
+                prereq = a.find('tr', {'class': 'prereqs'}).get_text().strip().replace('Prerequisite(s):', '').replace('\n','')
+            else:
+                prereq = "None"
+            
+            allCourseTitles.append(courseTitle)
+            allCourseCredits.append(courseCredit)
+            allCoursePrerequisites.append(prereq)
+            allCourseRestrictions.append(restrictions)
+    
+    courseData = pd.DataFrame(
+        {'Course Title': allCourseTitles,
+        'Course Credit': allCourseCredits,
+        'Course Prerequisites': allCoursePrerequisites,
+        'Course Restrictions': allCourseRestrictions
+        }
+    )
+    courseData.to_csv('courseData.csv')
 
 courseScraper()
-
